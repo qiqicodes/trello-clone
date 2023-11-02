4a237b8dffeed9665f49a0075a16c6274a2072b3
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-11-02 09:20:15 -0700

    installs icon deps, styles header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,32 @@
 import Image from "next/image";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/20/solid";
 
 function Header() {
   return (
     <header>
-      <Image
-        src="/trello.svg"
-        alt="Trello Logo"
-        width={300}
-        height={100}
-        className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
-      />
+      <div className="flex flex-col md:flex-row items-center p-5 bg-blue-400/10 rounded-b-l">
+        <Image
+          src="/Trello_logo.png"
+          alt="Trello Logo"
+          width={150}
+          height={50}
+          className="w-44 md:w-56 pb-10 md:pb-0"
+        />
 
-      <div>
-        {/* Search Box */}
-        {/* Avatar */}
+        <div className="flex items-center space-x-4 flex-1 justify-end w-full">
+          {/* Search Box */}
+          <form className="flex items-center space-x-4 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="flex-1 outline-none p-2"
+            />
+            <button hidden>Search</button>
+          </form>
+          {/* Avatar */}
+          <UserCircleIcon className="h-16 w-16 text-blue-500" />
+        </div>
       </div>
     </header>
   );
