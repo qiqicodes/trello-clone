b0807f6765b0a06dfcab649c64a92e4c437491ca
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-11-02 14:03:37 -0700

    install dnd deps, and lists Board component todos

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,5 +1,32 @@
+import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
+
 function Board() {
-  return <>{/* Drag and Drop */}</>;
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source, type, draggableId } = result;
+
+    // check if the user dragged card or column
+    // check if the user has dragged card outside of board and not into a column
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-3"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {/* render columns */}
+            {/* columns: to do, in progress, done */}
+            {/* each column has note cards */}
+
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
 
 export default Board;
