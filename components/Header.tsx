0104311c4ad368133cd582eb6e6bc1d65c01fff5
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-11-02 10:01:22 -0700

    styles header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,23 @@ import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/20/solid";
 function Header() {
   return (
     <header>
-      <div className="flex flex-col md:flex-row items-center p-5 bg-blue-400/10 rounded-b-l">
+      <div className="flex flex-col md:flex-row items-center p-4 bg-orange-400/10 rounded-b-l">
+        <div
+          className="
+          absolute
+          top-0
+          left-0
+          w-full
+          h-2/5
+          bg-gradient-to-br
+          from-orange-500
+          to-pink-300
+          filter
+          blur-3xl
+          opacity-90
+          -z-10
+          "
+        />
         <Image
           src="/Trello_logo.png"
           alt="Trello Logo"
@@ -14,8 +30,7 @@ function Header() {
         />
 
         <div className="flex items-center space-x-4 flex-1 justify-end w-full">
-          {/* Search Box */}
-          <form className="flex items-center space-x-4 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+          <form className="flex items-center space-x-4 bg-white rounded-md p-2 shadow-lg flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-500" />
             <input
               type="text"
@@ -24,8 +39,8 @@ function Header() {
             />
             <button hidden>Search</button>
           </form>
-          {/* Avatar */}
-          <UserCircleIcon className="h-16 w-16 text-blue-500" />
+          {/* Avatar - todo */}
+          <UserCircleIcon className="h-16 w-16 text-blue-500 " />
         </div>
       </div>
     </header>
