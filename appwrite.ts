b24b19c5760796fe36ddc35091dd13cc8f2f9d12
qiqicodes <68657634+qiqicodes@users.noreply.github.com>
2023-11-09 15:55:11 -0800

    sets up appwrite db and storage bucket

--- appwrite.ts
+++ appwrite.ts
@@ -1,9 +1,13 @@
-
-import { Client } from 'appwrite';
+import { Client, Account, Databases, Storage, ID } from "appwrite";
 
 const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
 
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, databases, storage, ID };
